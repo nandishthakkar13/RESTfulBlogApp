e068220f29f4309a8f47674bed9a9d9d41012068
Nandish Thakkar <56491599+nandishthakkar13@users.noreply.github.com>
2019-10-27 09:44:33 -0400

    Second Commit
    Added Create and Read functionality
    (A user can create a new blog and read each blog on a separate page)

--- README.md
+++ README.md
@@ -1,3 +1,25 @@
 # RESTfulBlogApp
  RESTful blog website
 
+# Blog Index
+* Setup the blog App
+* Create the Blog Model
+* Add Index route and template
+
+# Basic Layout
+* Add Header and Footer Partials
+* Include Semantic UI
+* Add Simple Nav Bar
+
+# Putting the C in CRUD
+* Add NEW route
+* Add NEW template
+* Add CREATE route
+* Add CREATE template
+
+# Show Time
+* Add Show route
+* Add Show template
+* Add links to show page
+* Style show template
+
--- app.js
+++ app.js
@@ -8,8 +8,83 @@ var mongoose =  require("mongoose");
 
 mongoose.set('useNewUrlParser', true);
 mongoose.set('useUnifiedTopology', true);
+
+//APP CONFIG
 mongoose.connect("mongodb://localhost/restful_blog_app");
 
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended:true}));
+
+//MONGOOSE/MODEL CONFIG
+var blogSchema = new mongoose.Schema({
+    title:String,
+    image:String,
+    body:String,
+    created:{type:Date, default:Date.now}
+});
+
+
+var Blog = mongoose.model("Blog",blogSchema);
+/*
+Blog.create({
+    title: "Test Blog",
+    image: "https://images.unsplash.com/photo-1486312338219-ce68d2c6f44d?ixlib=rb-1.2.1&auto=format&fit=crop&w=1052&q=80",
+    body: "This is a testing blog",
+})*/
+
+//RESTFUL ROUTES
+
+app.get("/", (req,res) => {
+    res.redirect("/blogs");
+})
+
+//INDEX ROUTE
+app.get("/blogs", (req,res) => {
+    Blog.find({}, (err,blogs) => {
+        if(err){
+            console.log("ERROR!");
+        }
+        else{
+            res.render("index",{blogs:blogs});
+        }
+    });
+ 
+})
+
+//NEW ROUTE
+app.get("/blogs/new", (req, res) => {
+    res.render("new");
+})
+
+//POST ROUTE (CREATE)
+app.post("/blogs",(req, res) => {
+    //create blog
+    Blog.create(req.body.blog, (err, newBlog) => {
+        if(err){
+            res.render("new");
+        }
+        else{
+            //redirect to the index pages
+            res.redirect("/");
+        }
+    })
+    
+})
+
+//SHOW ROUTE
+app.get("/blogs/:id",(req,res) => {
+    Blog.findById(req.params.id, (err,foundblog) => {
+        if(err){
+            res.redirect("/blogs");
+        }
+        else{
+            res.render("show",{blog:foundblog});
+        }
+    });
+    
+})
+
+app.listen(3000,() =>{
+    console.log("Listening on server");
+})
